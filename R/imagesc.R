54eb686225d1683bd68faba6683234af7fcc2347
zdm <zmorrissey@protonmail.com>
2019-07-12 19:00:09 -0500

    Updated imagesc.R to accept either matrix or data frame as input.

--- R/imagesc.R
+++ R/imagesc.R
@@ -1,17 +1,31 @@
 #' Plot matrix using ggplot2
 #'
 #' Plot matrix analagous to Matlab's imagesc function.
-#' @param df Matrix data that has been converted to tidy dataframe using convert_mat_df
+#' @param df Matrix data
 #' @keywords matrix ggplot2
 #' @export
 #' @examples
 #' foo <- convert_mat_df(matrix(data=rnorm(n=25), nrow=5, ncol=5))
 #' imagesc(foo)
 
-imagesc <- function(df) {
-    ggplot(df, aes(Var1, Var2, fill = value)) +
+imagesc <- function(df, scale_label = "") {
+    ## Check input data class
+    if(!any(class(df) == "data.frame")) {
+        if(class(df) == "matrix") {
+            df <- convert_mat_df(df)
+        } else {
+            stop("Input is not of class 'matrix' or 'data.frame'.")
+        }
+    }
+
+    ## Visualize
+    ggplot(df, aes(rev(Var1),
+                   Var2,
+                   fill = value)) +
         geom_tile() +
-        scale_fill_viridis() +
+        scale_fill_viridis(name = scale_label) +
         labs(x = "", y = "") +
-        theme(axis.text.x = element_text(angle = 90))
+        theme(axis.text = element_blank(),
+              axis.ticks = element_blank()) +
+        coord_equal()
 }
