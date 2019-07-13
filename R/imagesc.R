63d27be94011b3e78870babe696bbd593d36ee41
zdm <zmorrissey@protonmail.com>
2019-07-12 19:35:49 -0500

    Updated imagesc documentation

--- R/imagesc.R
+++ R/imagesc.R
@@ -2,13 +2,17 @@
 #'
 #' Plot matrix analagous to Matlab's imagesc function.
 #' @param df Matrix data
+#' @param scale_label Label to be applied to the colorbar scale (blank by default)
+#' @param cmap_option Choose colormap among viridis scales (default)
 #' @keywords matrix ggplot2
 #' @export
 #' @examples
 #' foo <- convert_mat_df(matrix(data=rnorm(n=25), nrow=5, ncol=5))
 #' imagesc(foo)
 
-imagesc <- function(df, scale_label = "") {
+imagesc <- function(df,
+                    scale_label = "",
+                    cmap_option = "viridis") {
     ## Check input data class
     if(!any(class(df) == "data.frame")) {
         if(class(df) == "matrix") {
@@ -23,8 +27,14 @@ imagesc <- function(df, scale_label = "") {
                    Var2,
                    fill = value)) +
         geom_tile() +
-        scale_fill_viridis(name = scale_label) +
+        scale_fill_viridis(name = scale_label, option = cmap_option) +
         labs(x = "", y = "") +
+        guides(fill = guide_colorbar(
+                   barwidth = 1,
+                   barheight = 15,
+                   label.theme = element_text(size = 12),
+                   draw.ulim = TRUE,
+                   draw.llim = TRUE)) +
         theme(axis.text = element_blank(),
               axis.ticks = element_blank()) +
         coord_equal()
