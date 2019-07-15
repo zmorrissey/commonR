cd6b20bda7b69c7caeb57d4161e47db1ecf7bf47
zdm <zmorrissey@protonmail.com>
2019-07-15 14:03:06 -0500

    Updated save_col.R parameters

--- R/save_col.R
+++ R/save_col.R
@@ -6,20 +6,22 @@
 #' @param col_width Column width (default = 1)
 #' @param path Save path (default is getwd() value)
 #' @param scale Scale of plot (default = 1)
+#' @export
 #' @examples
 #' p <- ggplot(mtcars, aes(mpg, wt)) + geom_point()
 #' save_col("mtcars-1col.png")  # single-column width
 #' save_col("mtcars-1.5col.png", col_width = "2")  # double-column width
 
 
-col_widths <- list("1" = 85,
-                   "1.5" = 114,
-                   "2" = 300)
-
 save_col <- function(title,
                      col_width = "1",
                      path = ".",
                      scale = 1) {
+
+    col_widths <- list("1" = 85,
+                       "1.5" = 114,
+                       "2" = 300)
+
     ggsave(filename = title,
            path = path,
            dpi = 300,
