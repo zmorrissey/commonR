d4ef4a4eb37ddad0b336a3e114ca6fddfbea61b8
zdm <zmorrissey@protonmail.com>
2019-07-15 13:56:15 -0500

    Updated quick-save.R functions to save_col.R

---
+++ R/save_col.R
@@ -0,0 +1,30 @@
+#' Save plots in 1-, 1.5-, or 2-column width for manuscripts
+#'
+#' Wraps ggsave to save plots in specified column widths for publication.
+#' @keywords save plot ggsave ggplot2
+#' @param title Title of plot
+#' @param col_width Column width (default = 1)
+#' @param path Save path (default is getwd() value)
+#' @param scale Scale of plot (default = 1)
+#' @examples
+#' p <- ggplot(mtcars, aes(mpg, wt)) + geom_point()
+#' save_col("mtcars-1col.png")  # single-column width
+#' save_col("mtcars-1.5col.png", col_width = "2")  # double-column width
+
+
+col_widths <- list("1" = 85,
+                   "1.5" = 114,
+                   "2" = 300)
+
+save_col <- function(title,
+                     col_width = "1",
+                     path = ".",
+                     scale = 1) {
+    ggsave(filename = title,
+           path = path,
+           dpi = 300,
+           width = col_widths[[col_width]],
+           height = 85,
+           scale = scale,
+           units = "mm")
+}
