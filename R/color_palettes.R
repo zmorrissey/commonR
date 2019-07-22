e41250ff011ccf79d006e37ac1b0128416fb7cd5
zdm <zmorrissey@protonmail.com>
2019-07-21 19:10:32 -0500

    Added matplotlib color palette

--- R/color_palettes.R
+++ R/color_palettes.R
@@ -1,7 +1,9 @@
-#' Color blind-friendly color palettes for plots
+#' Color palettes for plots
 #'
-#' Color blind-friendly palettes and plot colors I like. Palettes taken from
-#' http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#palettes-color-brewer
+#' Color palettes and plot colors I like. Color blind-friendly palettes taken
+#' from the Cookbook for R (http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#palettes-color-brewer).
+#' The Matplotlib default color palette was taken from the documentation (https://matplotlib.org/users/dflt_style_changes.html#colors-in-default-property-cycle).
+#'
 #' @keywords color palette theme ggplot2
 #' @export cb_palette
 #' @export cbb_palette
@@ -17,10 +19,22 @@
 #' @export cb_red
 #' @export cb_pink
 #' @export cb_black
+#' @export plt_blue
+#' @export plt_orange
+#' @export plt_green
+#' @export plt_red
+#' @export plt_purple
+#' @export plt_brown
+#' @export plt_pink
+#' @export plt_gray
+#' @export plt_yellow
+#' @export plt_teal
+#' @export matplotlib_palette
+#'
 #' @examples
 #' ggplot(mtcars, aes(wt, mpg)) + geom_point(aes(color = as.factor(cyl))) + scale_color_manual(values = cbPalette)
 
-# Color blind-friendly colors
+### Color blind-friendly colors
 cb_gray      <- "#999999"
 cb_orange    <- "#E69F00"
 cb_lightblue <- "#56B4E9"
@@ -31,7 +45,7 @@ cb_red       <- "#D55E00"
 cb_pink      <- "#CC79A7"
 cb_black     <- "#000000"
 
-# Palette with gray
+### Palette with gray
 cb_palette <- c(cb_gray,
                 cb_orange,
                 cb_lightblue,
@@ -41,7 +55,7 @@ cb_palette <- c(cb_gray,
                 cb_red,
                 cb_pink)
 
-# Palette with black
+### Palette with black
 cbb_palette <- c(cb_black,
                  cb_orange,
                  cb_lightblue,
@@ -52,7 +66,30 @@ cbb_palette <- c(cb_black,
                  cb_pink)
 
 
-# Individual colors to highlight certain plot elements
+### Matplotlib default color cycle
+plt_blue   <- "#1F77B4"
+plt_orange <- "#FF7F0E"
+plt_green  <- "#2CA02C"
+plt_red    <- "#D62728"
+plt_purple <- "#9467bd"
+plt_brown  <- "#8C564B"
+plt_pink   <- "#E377C2"
+plt_gray   <- "#7F7F7F"
+plt_yellow <- "#BCBD22"
+plt_teal   <- "#17BECF"
+
+matplotlib_palette <- c(plt_blue,
+                        plt_orange,
+                        plt_green,
+                        plt_red,
+                        plt_purple,
+                        plt_brown,
+                        plt_pink,
+                        plt_gray,
+                        plt_yellow,
+                        plt_teal)
+
+### Individual colors to highlight certain plot elements
 brickred <- "#B5251C"  # from LaTeX
 
 changred <- "#D12413"  # from "Physical Chemistry" by Raymond Chang
