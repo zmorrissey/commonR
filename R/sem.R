f444feaf2f4832332742a9a851e007793376c422
zdm <zmorrissey@protonmail.com>
2021-03-01 14:04:55 -0600

    Renamed se.R to sem.R

--- R/sem.R
+++ R/sem.R
@@ -1,12 +1,12 @@
 #' Compute standard error of the mean
 #'
-#' Easy function for computing standard error of the mean (how is this not in base R?).
+#' Convenience function for computing standard error of the mean.
 #' @param v Numerical vector to compute the standard error of.
 #' @keywords standard error
 #' @export
 #' @examples
-#' se(rnorm(n=50, mean=5, sd=1))
+#' sem(rnorm(n=50, mean=5, sd=1))
 
-se <- function(v) {
+sem <- function(v) {
     return(sd(v) / sqrt(length(v)))
 }
