4ad21553c3f3477513453a81eebccb38905399eb
zdm <zmorrissey@protonmail.com>
2021-09-09 10:58:29 -0500

    Updated save_pres default width and height.

--- R/save_pres.R
+++ R/save_pres.R
@@ -1,6 +1,9 @@
 #' Save plot using sizes optimal for (16:9) slide presentation.
 #'
 #' Wraps ggsave to save plots in specified column widths for presentation.
+#' Default height is set to the default height of the bullet textbox in
+#' LibreOffice Impress. Default width is calculated from golden ratio of the
+#' height.
 #' @keywords save plot ggsave ggplot2
 #' @param title Title of plot
 #' @param path Save path (default is getwd() value)
@@ -14,8 +17,8 @@ save_pres <- function(filename, scale = 1) {
     ggsave(
         filename = filename,
         dpi = 600,
-        width = 6,
-        height = 4,
+        width = 5.8248,
+        height = 3.606,
         scale = scale,
         units = "in")
 }
